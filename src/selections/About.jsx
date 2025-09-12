4c8691e58c7b9511c3b5bce673c7fbb9d90a3fa3
frdiskandr <fariditb159@gmail.com>
2025-09-12 12:06:02 +0700

    update public img

---
+++ src/selections/About.jsx
@@ -0,0 +1,70 @@
+import React, { useRef } from 'react'
+import Card from '../components/Card'
+import { Globe } from '../components/globe'
+import CopyEmailButton from '../components/CopyEmailButton'
+import { Frameworks } from '../components/FrameWorks'
+
+
+const About = () => {
+    const grid2Container = useRef()
+    return (
+        <section className='c-space section-spacing'>
+            <h2 className='text-heading'>About Me</h2>
+            <div className='grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12'>
+                {/* grid */}
+                <div className="flex items-end grid-default-color grid-1">
+                    <img src="assets/coding-pov.png" alt="coding-img" className='absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]' />
+                    <div className="z-10">
+                        <p className='headtext'>Hi, I'm Faried Iskandar</p>
+                        <p className='subtext'>Full-Stack Developer with 2+ years of experience building scalable web applications. Skilled in React.js, Vue.js, Golang, and Node.js,</p>
+                    </div>
+                    <div className='absolute inset-x-0 pointer-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo'></div>
+                </div>
+                {/* grid 2 */}
+                <div className="grid-default-color grid-2">
+                    <div ref={grid2Container} className="flex items-center justify-center w-full h-full">
+                        <p className='flex items-end text-5xl text-gray-500'>
+                            CODE IS ELEGANT
+                        </p>
+                        <Card
+                            style={{ rotate: '75deg', top: '40%', left: '60%' }}
+                            text={"Javascripts"}
+                            containerRef={grid2Container}
+                        />
+                        <Card
+                            style={{ rotate: '45deg', top: '20%', left: '70%' }}
+                            text={"Golang"}
+                            containerRef={grid2Container} />
+                    </div>
+                </div>
+                {/* grid 3 */}
+                <div className="grid-black-color grid-3">
+                    <div className="z-10 w-[50%]">
+                        <p className="headtext">Location</p>
+                        <p className='subtext'>I'm From Bogor. <br/> West java, Indonesia.</p>
+                    </div>
+                    <figure className='absolute left-[30%] top-[10%]'>
+                        {/* <Globe/> */}
+                    </figure>
+                </div>
+                {/* grid 4 */}
+                <div className="grid-special-color grid-4">
+                    <div className="flex flex-col items-center justify-center gap-4 size-full">
+                        <p className="text-center headtext">
+                            DO you want start a project together?
+                        </p>
+                        <CopyEmailButton/>
+                    </div>
+                </div>
+                {/* grid 5 */}
+                <div className="grid-default-color grid-5">
+                    <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
+                        <Frameworks/>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About
